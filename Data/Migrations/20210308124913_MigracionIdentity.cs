bf222c6d4c5765e6f7609000d90ad1b0ecf4f06d
jignaciopereira <pereira.ignacio.96@gmail.com>
2021-03-08 09:56:48 -0300

    MigracionBase

---
+++ Data/Migrations/20210308124913_MigracionIdentity.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MusicShopMVC.Data.Migrations
+{
+    public partial class MigracionIdentity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
